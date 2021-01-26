47ffd449f495b6e2d5fc08a7bc41a2b59e077e0e
Miguel Barba <barbah.miguel@gmail.com>
2021-01-26 15:34:57 -0800

    Hello World

---
+++ CouchCoach/CouchCoach/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CouchCoach
+//
+//  Created by Miguel Barba on 1/26/21.
+//  Copyright © 2021 GetFit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
