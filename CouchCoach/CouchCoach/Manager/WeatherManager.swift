b0497bebe85cd1ed12312071b401fe095c2e7612
Andrew Pham <andrewpham@Andrews-MacBook-Pro.local>
2021-03-09 15:30:58 -0800

    Open Weather Map API

---
+++ CouchCoach/CouchCoach/Manager/WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  CouchCoach
+//
+//  Created by Andrew Pham on 3/9/21.
+//  Copyright © 2021 GetFit. All rights reserved.
+//
+
+import Foundation
---
+++ CouchCoach/CouchCoach/Weather/CurrentWeather.swift
@@ -0,0 +1,9 @@
+//
+//  CurrentWeather.swift
+//  CouchCoach
+//
+//  Created by Andrew Pham on 3/9/21.
+//  Copyright © 2021 GetFit. All rights reserved.
+//
+
+import Foundation
