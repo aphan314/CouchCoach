f04d17e104008dab89616cdc3feb0961b5dd4f8c
Andrew Pham <andrewpham@Andrews-MacBook-Pro.local>
2021-03-09 15:35:32 -0800

    open weather map API

--- CouchCoach/CouchCoach/Manager/WeatherManager.swift
+++ CouchCoach/CouchCoach/Manager/WeatherManager.swift
@@ -7,3 +7,59 @@
 //
 
 import Foundation
+
+struct Weather {
+    var city                    : String?
+    var temp                    : Double?
+    var condition               : String?
+    var conditionDesc           : String?
+}
+
+class WeatherManager {
+    static let shared = WeatherManager()
+    func retrieveWeather(lat: Double, lon: Double) -> Weather {
+        
+        let apikey = ""
+        let baseUrl = "http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apikey)&units=imperial"
+        
+        let url = URL(string:baseUrl)
+        
+        var request = URLRequest(url: url!)
+        request.httpMethod = "GET"
+        
+        var weather = Weather()
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            do {
+                
+                guard let data = data else {
+                    return
+                }
+                
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                
+                guard let resp = json as? NSDictionary else { return }
+            
+                guard let main = resp.value(forKey: "main") as? NSDictionary else {return}
+           
+                guard let con = resp.value(forKey: "weather") as? [NSDictionary] else {return}
+      
+                
+                weather.city = resp.value(forKey: "name") as? String
+                weather.condition = con[0].value(forKey: "main") as? String
+                weather.conditionDesc = con[0].value(forKey: "description") as? String
+                weather.temp = main.value(forKey: "temp") as? Double
+            
+                print(weather)
+     
+            } catch {
+                print("Caught error")
+            }
+            }.resume()
+        
+        return weather
+        
+    }
+    
+}
+
+//(lat: 33.684566, lon:-117.826508)
--- CouchCoach/CouchCoach/Weather/CurrentWeather.swift
+++ CouchCoach/CouchCoach/Weather/CurrentWeather.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+
+class CurrentWeather{
+    var lat: Double = 0.0
+    var lon: Double = 0.0
+    var temp: Double = 0.0
+    var condition: String = ""
+    var conditionDesc: String = ""
+    var city: String = ""
+    
+    init()
+    {
+        self.lat = Double(LocationManager.shared.lastLocation?.coordinate.latitude ?? 0)
+        self.lon = Double(LocationManager.shared.lastLocation?.coordinate.longitude ?? 0)
+        
+        let w = WeatherManager.shared.retrieveWeather(lat:self.lat, lon:self.lon)
+        
+        city = w.city ?? "Unknown"
+        temp = w.temp ?? 0
+        condition = w.condition ?? "Clear"
+        conditionDesc = w.conditionDesc ?? "Clear Skies"
+        
+    }
+    
+    func updateWeather() {
+        self.lat = Double(LocationManager.shared.lastLocation?.coordinate.latitude ?? 0)
+        self.lon = Double(LocationManager.shared.lastLocation?.coordinate.longitude ?? 0)
+        
+        let w = WeatherManager.shared.retrieveWeather(lat:self.lat, lon:self.lon)
+        
+        city = w.city ?? "Unknown"
+        temp = w.temp ?? 0
+        condition = w.condition ?? "Clear"
+        conditionDesc = w.conditionDesc ?? "Clear Skies"
+        
+    }
+    
+    func getCity() -> String {
+        return city
+    }
+    
+    func getTemp() -> Double {
+        return temp
+    }
+    
+    func getCond() -> String {
+        return condition
+    }
+    
+    func getCondDesc() -> String{
+        return conditionDesc
+    }
+
+}
